179c3e82079e68cbbe11a7443f6db1c04e1f38d8
Adalkenny <adalbertomascarenhas1@gmail.com>
2019-02-07 18:51:59 -0100

    new enginee

--- src/safety/controller/ServicosController.java
+++ src/safety/controller/ServicosController.java
@@ -15,7 +15,6 @@ import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.CheckBox;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.CheckBoxTableCell;
@@ -96,6 +95,7 @@ public class ServicosController implements Initializable {
         
         colunElim.setCellFactory(CheckBoxTableCell.forTableColumn(colunElim));
         colunDescricao.setCellFactory(TextFieldTableCell.forTableColumn());
+        
 
         tableview.setItems(loadTable());
         getSelectRow();
